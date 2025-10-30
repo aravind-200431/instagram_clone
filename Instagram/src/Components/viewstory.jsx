cad3c204dbb35d73704be622dcc0e2e895b26833
aravind-200431 <daravindsuresh@gmail.com>
2025-10-30 16:18:09 +0530

    add dynamic path to story

--- Instagram/src/Components/viewstory.jsx
+++ Instagram/src/Components/viewstory.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import
+import { useState,useEffect } from 'react'
 
 function Viewstory() {
   const { id } = useParams();
   const [storydata, setStorydata] = useState(null);
-  
+useEffect(
+  ()=>
+  {
+    fetch(`http://localhost:3002/story/${id}`)
+    .then((res)=>res.json())
+    .then((data)=>setStorydata(data))
+    .catch((err)=>console.error("Error fetching story:",err))
+  },[id]);
+
+
   return (
+    
     <div>
-       viewstory {id}
+       {
+      storydata?<div><img src={storydata.story_pic} alt='story' /></div> : <div>Loading...</div>
+       }
     </div>
   )
 }
-
 export default Viewstory
